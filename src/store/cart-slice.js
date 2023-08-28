9f53ae2fb136cc02a7976e4ea2e9ea492d694ebb
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-28 13:58:33 +0300

    created a slice to show items that are in the cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,1 +1,28 @@
-import { CreateSliceOptions } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+  name: "cartItems",
+  initialState: { items: [], totalQuantity: 0, totalAmount: 0 },
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      //   if not in the cart
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalAmount: newItem.Price,
+          title: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalAmount += newItem.Price;
+      }
+
+      //   check if item exists in our items array
+    },
+    removeFromCart() {},
+  },
+});
