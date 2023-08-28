43015b191310ab7196756c3edf6922d34ac8f059
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-28 14:26:20 +0300

    added dummy data to the products section

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
-
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 20.45,
+    title: "My first book",
+    description: "This is an amazing book",
+  },
+  {
+    id: "p2",
+    price: 40.45,
+    title: "My second book",
+    description: "This is also an amazing book",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => {
+          return (
+            <ProductItem
+              title={item.title}
+              id={item.id}
+              price={item.price}
+              description={item.description}
+            />
+          );
+        })}
       </ul>
     </section>
   );
